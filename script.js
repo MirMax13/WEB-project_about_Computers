0760a12b77c986056a4fa2b37e108071e7ea9cb8
Maxym Matskiv <matskiv2005@gmail.com>
2024-03-05 13:42:56 +0200

    05.03 13:42 Refactor menu functionality in script.js and index.html

--- script.js
+++ script.js
@@ -12,20 +12,12 @@ window.addEventListener('DOMContentLoaded', function() {
     } else {
       console.error('iframe element not found');
     }
+  
+  const menuBtn = document.getElementById('menuBtn');
+  const navLinks = document.getElementById('navLinks');
+  
+  menuBtn.addEventListener('click', () => {
+    navLinks.classList.toggle('show');
+  });
 });
 
-window.addEventListener('DOMContentLoaded', function() {
-  const menuItems = document.querySelectorAll('.menu-item a');
-  
-  function resizeMenuItems() {
-    const screenWidth = window.innerWidth;
-    const width = Math.max(5, screenWidth);
-  
-    for (const menuItem of menuItems) {
-      menuItem.style.width = `${width*0.889}px`;
-    }
-  }
-  
-  window.addEventListener('resize', resizeMenuItems);
-  resizeMenuItems();
-});
